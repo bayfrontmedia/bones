3e5d89b3d02e5d366f7909e075f141e2cd91d417
John Robinson <john@bayfrontmedia.com>
2023-05-16 08:39:52 -0400

    Added TenantGroupUsers

--- src/Services/Api/Controllers/Relationships/TenantGroupUsersController.php
+++ src/Services/Api/Controllers/Relationships/TenantGroupUsersController.php
@@ -2,7 +2,122 @@
 
 namespace Bayfront\Bones\Services\Api\Controllers\Relationships;
 
-class TenantGroupUsersController
+use Bayfront\ArraySchema\InvalidSchemaException;
+use Bayfront\Bones\Application\Services\EventService;
+use Bayfront\Bones\Application\Services\FilterService;
+use Bayfront\Bones\Application\Utilities\App;
+use Bayfront\Bones\Exceptions\HttpException;
+use Bayfront\Bones\Services\Api\Controllers\Abstracts\ApiController;
+use Bayfront\Bones\Services\Api\Controllers\Interfaces\RelationshipInterface;
+use Bayfront\Bones\Services\Api\Exceptions\BadRequestException;
+use Bayfront\Bones\Services\Api\Exceptions\NotFoundException;
+use Bayfront\Bones\Services\Api\Exceptions\UnexpectedApiException;
+use Bayfront\Bones\Services\Api\Models\Relationships\TenantGroupUsersModel;
+use Bayfront\Bones\Services\Api\Schemas\Resources\UsersCollection;
+use Bayfront\Container\NotFoundException as ContainerNotFoundException;
+use Bayfront\HttpRequest\Request;
+use Bayfront\HttpResponse\InvalidStatusCodeException;
+use Bayfront\HttpResponse\Response;
+
+class TenantGroupUsersController extends ApiController implements RelationshipInterface
 {
+
+    protected TenantGroupUsersModel $tenantGroupUsersModel;
+
+    public function __construct(EventService $events, FilterService $filters, Response $response, TenantGroupUsersModel $tenantGroupUsersModel)
+    {
+        $this->tenantGroupUsersModel = $tenantGroupUsersModel;
+
+        parent::__construct($events, $filters, $response);
+    }
+
+    /**
+     * Add users to tenant group.
+     *
+     * @param array $args
+     * @return void
+     * @throws ContainerNotFoundException
+     * @throws HttpException
+     * @throws UnexpectedApiException
+     * @throws InvalidStatusCodeException
+     */
+    public function add(array $args): void
+    {
+
+        $ids = $this->getToManyRelationshipIdsOrAbort('users');
+
+        try {
+
+            $this->tenantGroupUsersModel->add($args['tenant_id'], $args['group_id'], $ids);
+
+        } catch (BadRequestException $e) {
+            App::abort(400, $e->getMessage());
+        } catch (NotFoundException $e) {
+            App::abort(404, $e->getMessage());
+        }
+
+        $this->response->setStatusCode(204)->send();
+
+    }
+
+    /**
+     * Get tenant group users collection.
+     *
+     * @param array $args
+     * @return void
+     * @throws ContainerNotFoundException
+     * @throws HttpException
+     * @throws InvalidSchemaException
+     * @throws InvalidStatusCodeException
+     * @throws UnexpectedApiException
+     */
+    public function getCollection(array $args): void
+    {
+
+        $query = $this->parseCollectionQueryOrAbort(Request::getQuery(), 'users');
+
+        try {
+
+            $results = $this->tenantGroupUsersModel->getCollection($args['tenant_id'], $args['group_id'], $query);
+
+        } catch (BadRequestException $e) {
+            App::abort(400, $e->getMessage());
+        } catch (NotFoundException $e) {
+            App::abort(404, $e->getMessage());
+        }
+
+        $schema = UsersCollection::create($results, [
+            'collection_prefix' => '/users',
+            'query_string' => Request::getQuery()
+        ]);
+
+        $this->response->setStatusCode(200)->sendJson($this->filters->doFilter('api.response', $schema));
+
+    }
+
+    /**
+     * Remove users from tenant group.
+     *
+     * @param array $args
+     * @return void
+     * @throws ContainerNotFoundException
+     * @throws HttpException
+     * @throws InvalidStatusCodeException
+     * @throws UnexpectedApiException
+     */
+    public function remove(array $args): void
+    {
+
+        $ids = $this->getToManyRelationshipIdsOrAbort('users');
+
+        try {
+
+            $this->tenantGroupUsersModel->remove($args['tenant_id'], $args['group_id'], $ids);
+
+        } catch (NotFoundException $e) {
+            App::abort(404, $e->getMessage());
+        }
+
+    }
 
 }
