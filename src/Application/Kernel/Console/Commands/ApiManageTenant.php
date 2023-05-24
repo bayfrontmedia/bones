d575ee1562459eed3eda5d4955c2c75ec1c47670
John Robinson <john@bayfrontmedia.com>
2023-05-24 07:53:32 -0400

    Update manage tenant command

--- src/Application/Kernel/Console/Commands/ApiManageTenant.php
+++ src/Application/Kernel/Console/Commands/ApiManageTenant.php
@@ -2,17 +2,20 @@
 
 namespace Bayfront\Bones\Application\Kernel\Console\Commands;
 
+use Bayfront\ArrayHelpers\Arr;
+use Bayfront\Bones\Application\Utilities\App;
 use Bayfront\Bones\Services\Api\Models\Relationships\TenantRolePermissionsModel;
 use Bayfront\Bones\Services\Api\Models\Resources\TenantMetaModel;
 use Bayfront\Bones\Services\Api\Models\Resources\TenantPermissionsModel;
 use Bayfront\Bones\Services\Api\Models\Resources\TenantRolesModel;
 use Bayfront\Bones\Services\Api\Models\Resources\TenantsModel;
+use Exception;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ChoiceQuestion;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Question\Question;
 
 class ApiManageTenant extends Command
 {
@@ -100,8 +103,159 @@ class ApiManageTenant extends Command
     private function createTenant(InputInterface $input, OutputInterface $output): int
     {
 
-        $output->writeln('Create tenant');
+        $helper = $this->getHelper('question');
 
+        $question = new Question('Enter owner ID: ');
+        $owner = $helper->ask($input, $output, $question);
+
+        $question = new Question('Enter tenant name: ');
+        $name = $helper->ask($input, $output, $question);
+
+        $meta = [];
+
+        foreach (App::getConfig('api.required_meta.tenants') as $k => $v) {
+
+            $question = new Question('Enter required meta (' . $k . '): ');
+            $meta[$k] = $helper->ask($input, $output, $question);
+
+        }
+
+        $enabled = false;
+
+        $question = new ConfirmationQuestion('Enable tenant? [y/n] ', false);
+
+        if ($helper->ask($input, $output, $question)) {
+            $enabled = true;
+        }
+
+        $helper = $this->getHelper('question');
+        $question = new ChoiceQuestion(
+            'Choose a plan: ', array_keys(App::getConfig('api-plans')),
+            0
+        );
+
+        $question->setErrorMessage('Plan %s is invalid.');
+        $plan = $helper->ask($input, $output, $question);
+
+        $output->writeLn('<info>Ready to create tenant:</info>');
+        $output->writeLn('<question>Owner ID:</question> ' . $owner);
+        $output->writeLn('<question>Name:</question> ' . $name);
+
+        foreach ($meta as $k => $v) {
+            $output->writeLn('<question>Meta (' . $k . '):</question> ' . $v);
+        }
+
+        if ($enabled) {
+            $output->writeLn('<question>Enabled:</question> True');
+        } else {
+            $output->writeLn('<question>Enabled:</question> False');
+        }
+
+        $output->writeLn('<question>Plan:</question> ' . $plan);
+
+        $question = new ConfirmationQuestion('Are you sure you want to proceed? [y/n] ', false);
+
+        if (!$helper->ask($input, $output, $question)) {
+            return Command::SUCCESS;
+        }
+
+        // Create tenant
+
+        $attrs = [
+            'owner' => $owner,
+            'name' => $name,
+            'enabled' => $enabled
+        ];
+
+        if (!empty($meta)) {
+            $attrs['meta'] = $meta;
+        }
+
+        try {
+
+            $tenant_id = $this->tenantsModel->create($attrs);
+
+        } catch (Exception $e) {
+
+            $output->writeln('<error>' . $e->getMessage() . '</error>');
+            return Command::FAILURE;
+
+        }
+
+        /*
+         * Create permissions and roles based on plan
+         */
+
+        $permission_ids = [];
+
+        $plan = App::getConfig('api-plans.' . $plan, []);
+
+        foreach (Arr::get($plan, 'permissions', []) as $permission => $description) {
+
+            try {
+
+                $permission_ids[$permission] = $this->tenantPermissionsModel->create($tenant_id, [
+                    'name' => $permission,
+                    'description' => $description
+                ]);
+
+            } catch (Exception $e) {
+
+                $output->writeln('<error>' . $e->getMessage() . '</error>');
+                return Command::FAILURE;
+
+            }
+
+        }
+
+        $role_ids = [];
+
+        foreach (Arr::get($plan, 'roles', []) as $role) {
+
+            try {
+
+                $id = $this->tenantRolesModel->create($tenant_id, [
+                    'name' => $role['name'],
+                    'description' => $role['description']
+                ]);
+
+                $role_ids[$role['name']] = $id;
+
+                $this->tenantRolePermissionsModel->add($tenant_id, $id, Arr::only($permission_ids, array_keys($role['permissions'])));
+
+            } catch (Exception $e) {
+
+                $output->writeln('<error>' . $e->getMessage() . '</error>');
+                return Command::FAILURE;
+
+            }
+
+        }
+
+        /*
+         * Save tenant meta based on plan
+         */
+
+        foreach (Arr::get($plan, 'tenant_meta', []) as $meta_id => $meta_value) {
+
+            try {
+
+                $this->tenantMetaModel->create($tenant_id, [
+                    'id' => $meta_id,
+                    'metaValue' => $meta_value
+                ], true);
+
+            } catch (Exception $e) {
+
+                $output->writeln('<error>' . $e->getMessage() . '</error>');
+                return Command::FAILURE;
+
+            }
+
+        }
+
+        $output->writeLn('<info>Tenant successfully created!</info>');
+        $output->writeLn('<info>ID: ' . $tenant_id . '</info>');
         return Command::SUCCESS;
 
     }
