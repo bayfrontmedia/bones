b6cb67739aa28f54b3f295962fc2415cf036a4aa
John Robinson <john@bayfrontmedia.com>
2023-05-25 08:23:46 -0400

    ApiManageTenant updates

--- src/Application/Kernel/Console/Commands/ApiManageTenant.php
+++ src/Application/Kernel/Console/Commands/ApiManageTenant.php
@@ -1,4 +1,4 @@
-<?php
+<?php /** @noinspection DuplicatedCode */
 
 namespace Bayfront\Bones\Application\Kernel\Console\Commands;
 
@@ -44,7 +44,7 @@ class ApiManageTenant extends Command
     private const OPTION_DISABLE = 'Disable tenant';
     private const OPTION_ENABLE = 'Enable tenant';
     private const OPTION_CHANGE_PLAN = 'Change tenant plan';
-    private const OPTION_REMOVE = 'Remove tenant';
+    private const OPTION_DELETE = 'Delete tenant';
 
     /**
      * @return void
@@ -82,7 +82,7 @@ class ApiManageTenant extends Command
                 self::OPTION_DISABLE,
                 self::OPTION_ENABLE,
                 self::OPTION_CHANGE_PLAN,
-                self::OPTION_REMOVE
+                self::OPTION_DELETE
         ],
             0
         );
@@ -98,8 +98,8 @@ class ApiManageTenant extends Command
             return $this->enableTenant($input, $output);
         } else if ($option == self::OPTION_CHANGE_PLAN) {
             return $this->changeTenantPlan($input, $output);
-        } else if ($option == self::OPTION_REMOVE) {
-            return $this->removeTenant($input, $output);
+        } else if ($option == self::OPTION_DELETE) {
+            return $this->deleteTenant($input, $output);
         } else {
             $output->writeln('<error>Unknown option</error>');
             return Command::FAILURE;
@@ -143,7 +143,6 @@ class ApiManageTenant extends Command
             $enabled = true;
         }
 
-        $helper = $this->getHelper('question');
         $question = new ChoiceQuestion(
             'Choose a plan: ', array_keys(App::getConfig('api-plans')),
             0
@@ -196,6 +195,333 @@ class ApiManageTenant extends Command
             return Command::FAILURE;
 
         }
+
+        $set_plan = $this->setTenantPlan($output, $tenant_id, $plan);
+
+        if ($set_plan === Command::FAILURE) {
+            return Command::FAILURE;
+        }
+
+        /*
+         * Create permissions and roles based on plan
+         */
+
+        /*
+
+        $permission_ids = [];
+
+        $plan = App::getConfig('api-plans.' . $plan, []);
+
+        foreach (Arr::get($plan, 'permissions', []) as $permission => $description) {
+
+            try {
+
+                $permission_ids[$permission] = $this->tenantPermissionsModel->create($tenant_id, [
+                    'name' => $permission,
+                    'description' => $description
+                ]);
+
+            } catch (Exception $e) {
+
+                $output->writeln('<error>' . $e->getMessage() . '</error>');
+                return Command::FAILURE;
+
+            }
+
+        }
+
+        $role_ids = [];
+
+        foreach (Arr::get($plan, 'roles', []) as $role) {
+
+            try {
+
+                $id = $this->tenantRolesModel->create($tenant_id, [
+                    'name' => $role['name'],
+                    'description' => $role['description']
+                ]);
+
+                $role_ids[$role['name']] = $id;
+
+                $this->tenantRolePermissionsModel->add($tenant_id, $id, Arr::only($permission_ids, array_keys($role['permissions'])));
+
+            } catch (Exception $e) {
+
+                $output->writeln('<error>' . $e->getMessage() . '</error>');
+                return Command::FAILURE;
+
+            }
+
+        }
+
+        /*
+         * Save plan-defined tenant meta
+         * /
+
+        foreach (Arr::get($plan, 'tenant_meta', []) as $meta_id => $meta_value) {
+
+            try {
+
+                $this->tenantMetaModel->create($tenant_id, [
+                    'id' => $meta_id,
+                    'metaValue' => $meta_value
+                ], true);
+
+            } catch (Exception $e) {
+
+                $output->writeln('<error>' . $e->getMessage() . '</error>');
+                return Command::FAILURE;
+
+            }
+
+        }
+
+        /*
+         * Save plan permission and role ID's
+         * /
+
+        try {
+
+            $this->tenantMetaModel->create($tenant_id, [
+                'id' => '00-plan-permissions',
+                'metaValue' => json_encode($permission_ids)
+            ], true);
+
+            $this->tenantMetaModel->create($tenant_id, [
+                'id' => '00-plan-roles',
+                'metaValue' => json_encode($role_ids)
+            ], true);
+
+        } catch (Exception $e) {
+
+            $output->writeln('<error>' . $e->getMessage() . '</error>');
+            return Command::FAILURE;
+
+        }
+
+        */
+
+        $output->writeLn('<info>Tenant successfully created!</info>');
+        $output->writeLn('<info>ID: ' . $tenant_id . '</info>');
+
+        return Command::SUCCESS;
+
+    }
+
+    /**
+     * Disable tenant.
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     * @noinspection PhpPossiblePolymorphicInvocationInspection
+     */
+    private function disableTenant(InputInterface $input, OutputInterface $output): int
+    {
+
+        $helper = $this->getHelper('question');
+
+        $question = new Question('Enter tenant ID: ');
+        $tenant_id = $helper->ask($input, $output, $question);
+
+        try {
+
+            $tenant = $this->tenantsModel->get($tenant_id, [
+                'name'
+            ]);
+
+        } catch (Exception $e) {
+
+            $output->writeln('<error>' . $e->getMessage() . '</error>');
+            return Command::FAILURE;
+
+        }
+
+        $question = new ConfirmationQuestion('Are you sure you want to disable this tenant (' . $tenant['name'] . ')? [y/n] ', false);
+
+        if (!$helper->ask($input, $output, $question)) {
+            return Command::SUCCESS;
+        }
+
+        try {
+
+            $this->tenantsModel->update($tenant_id, [
+                'enabled' => false
+            ]);
+
+        } catch (Exception $e) {
+
+            $output->writeln('<error>' . $e->getMessage() . '</error>');
+            return Command::FAILURE;
+
+        }
+
+        $output->writeLn('<info>Tenant (' . $tenant['name'] . ') successfully disabled</info>');
+        return Command::SUCCESS;
+
+    }
+
+    /**
+     * Enable tenant.
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     * @noinspection PhpPossiblePolymorphicInvocationInspection
+     */
+    private function enableTenant(InputInterface $input, OutputInterface $output): int
+    {
+
+        $helper = $this->getHelper('question');
+
+        $question = new Question('Enter tenant ID: ');
+        $tenant_id = $helper->ask($input, $output, $question);
+
+        try {
+
+            $tenant = $this->tenantsModel->get($tenant_id, [
+                'name'
+            ]);
+
+        } catch (Exception $e) {
+
+            $output->writeln('<error>' . $e->getMessage() . '</error>');
+            return Command::FAILURE;
+
+        }
+
+        $question = new ConfirmationQuestion('Are you sure you want to enable this tenant (' . $tenant['name'] . ')? [y/n] ', false);
+
+        if (!$helper->ask($input, $output, $question)) {
+            return Command::SUCCESS;
+        }
+
+        try {
+
+            $this->tenantsModel->update($tenant_id, [
+                'enabled' => true
+            ]);
+
+        } catch (Exception $e) {
+
+            $output->writeln('<error>' . $e->getMessage() . '</error>');
+            return Command::FAILURE;
+
+        }
+
+        $output->writeLn('<info>Tenant (' . $tenant['name'] . ') successfully enabled</info>');
+        return Command::SUCCESS;
+
+    }
+
+    private function changeTenantPlan(InputInterface $input, OutputInterface $output): int
+    {
+
+        $helper = $this->getHelper('question');
+
+        $question = new Question('Enter tenant ID: ');
+        $tenant_id = $helper->ask($input, $output, $question);
+
+        try {
+
+            $tenant = $this->tenantsModel->get($tenant_id, [
+                'name'
+            ]);
+
+        } catch (Exception $e) {
+
+            $output->writeln('<error>' . $e->getMessage() . '</error>');
+            return Command::FAILURE;
+
+        }
+
+        $question = new ConfirmationQuestion('Are you sure you want to change the plan of this tenant (' . $tenant['name'] . ')? [y/n] ', false);
+
+        if (!$helper->ask($input, $output, $question)) {
+            return Command::SUCCESS;
+        }
+
+        $question = new ChoiceQuestion(
+            'Choose a plan: ', array_keys(App::getConfig('api-plans')),
+            0
+        );
+
+        $question->setErrorMessage('Plan %s is invalid.');
+        $plan = $helper->ask($input, $output, $question);
+
+        $output->writeLn('<info>Ready to change tenant plan:</info>');
+        $output->writeLn('<question>Tenant ID:</question> ' . $tenant_id);
+        $output->writeLn('<question>Tenant name:</question> ' . $tenant['name']);
+
+        // Current plan
+
+        // New plan
+
+    }
+
+    /**
+     * Delete tenant.
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     * @noinspection PhpPossiblePolymorphicInvocationInspection
+     */
+    private function deleteTenant(InputInterface $input, OutputInterface $output): int
+    {
+
+        $helper = $this->getHelper('question');
+
+        $question = new Question('Enter tenant ID: ');
+        $tenant_id = $helper->ask($input, $output, $question);
+
+        try {
+
+            $tenant = $this->tenantsModel->get($tenant_id, [
+                'name'
+            ]);
+
+        } catch (Exception $e) {
+
+            $output->writeln('<error>' . $e->getMessage() . '</error>');
+            return Command::FAILURE;
+
+        }
+
+        $question = new ConfirmationQuestion('Are you sure you want to delete this tenant (' . $tenant['name'] . ')? [y/n] ', false);
+
+        if (!$helper->ask($input, $output, $question)) {
+            return Command::SUCCESS;
+        }
+
+        try {
+
+            $this->tenantsModel->delete($tenant_id);
+
+        } catch (Exception $e) {
+
+            $output->writeln('<error>' . $e->getMessage() . '</error>');
+            return Command::FAILURE;
+
+        }
+
+        $output->writeLn('<info>Tenant (' . $tenant['name'] . ') successfully deleted</info>');
+        return Command::SUCCESS;
+
+    }
+
+    /**
+     * Set/update tenant plan.
+     *
+     * @param OutputInterface $output
+     * @param string $tenant_id
+     * @param string $plan
+     * @return int
+     */
+    private function setTenantPlan(OutputInterface $output, string $tenant_id, string $plan): int
+    {
+
+        // TODO: Get current permissions/roles and update as necessary
 
         /*
          * Create permissions and roles based on plan
@@ -258,7 +584,7 @@ class ApiManageTenant extends Command
                 $this->tenantMetaModel->create($tenant_id, [
                     'id' => $meta_id,
                     'metaValue' => $meta_value
-                ], true);
+                ], true, true);
 
             } catch (Exception $e) {
 
@@ -278,12 +604,12 @@ class ApiManageTenant extends Command
             $this->tenantMetaModel->create($tenant_id, [
                 'id' => '00-plan-permissions',
                 'metaValue' => json_encode($permission_ids)
-            ], true);
+            ], true, true);
 
             $this->tenantMetaModel->create($tenant_id, [
                 'id' => '00-plan-roles',
                 'metaValue' => json_encode($role_ids)
-            ], true);
+            ], true, true);
 
         } catch (Exception $e) {
 
@@ -291,44 +617,6 @@ class ApiManageTenant extends Command
             return Command::FAILURE;
 
         }
-
-        $output->writeLn('<info>Tenant successfully created!</info>');
-        $output->writeLn('<info>ID: ' . $tenant_id . '</info>');
-        return Command::SUCCESS;
-
-    }
-
-    private function disableTenant(InputInterface $input, OutputInterface $output): int
-    {
-
-        $output->writeln('Disable tenant');
-
-        return Command::SUCCESS;
-
-    }
-
-    private function enableTenant(InputInterface $input, OutputInterface $output): int
-    {
-
-        $output->writeln('Enable tenant');
-
-        return Command::SUCCESS;
-
-    }
-
-    private function changeTenantPlan(InputInterface $input, OutputInterface $output): int
-    {
-
-        $output->writeln('Change tenant plan');
-
-        return Command::SUCCESS;
-
-    }
-
-    private function removeTenant(InputInterface $input, OutputInterface $output): int
-    {
-
-        $output->writeln('Remove tenant');
 
         return Command::SUCCESS;
 
