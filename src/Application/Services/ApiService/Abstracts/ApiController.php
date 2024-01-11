624735b1e19f85bdd9b274353b18aa8d90ff985b
John Robinson <john@bayfrontmedia.com>
2024-01-11 10:35:57 -0500

    Moved filters to ApiService

--- src/Application/Services/ApiService/Abstracts/ApiController.php
+++ src/Application/Services/ApiService/Abstracts/ApiController.php
@@ -5,18 +5,17 @@ namespace Bayfront\Bones\Application\Services\ApiService\Abstracts;
 use Bayfront\Bones\Abstracts\Controller;
 use Bayfront\Bones\Application\Services\ApiService\ApiService;
 use Bayfront\Bones\Application\Services\ApiService\Interfaces\ApiControllerInterface;
-use Bayfront\Bones\Application\Services\Events\EventService;
 
 abstract class ApiController extends Controller implements ApiControllerInterface
 {
 
     protected ApiService $apiService;
 
-    public function __construct(EventService $events, ApiService $apiService)
+    public function __construct(ApiService $apiService)
     {
         $this->apiService = $apiService;
 
-        parent::__construct($events);
+        parent::__construct($this->apiService->events);
     }
 
 }
--- src/Application/Services/ApiService/ApiService.php
+++ src/Application/Services/ApiService/ApiService.php
@@ -3,17 +3,34 @@
 namespace Bayfront\Bones\Application\Services\ApiService;
 
 use Bayfront\Bones\Application\Services\Events\EventService;
+use Bayfront\Bones\Application\Services\Filters\FilterService;
+use Bayfront\HttpResponse\Response;
 
 class ApiService
 {
 
-    protected EventService $events;
+    public EventService $events; // Needed by the Bones abstract controller (public)
+    protected FilterService $filters;
+    protected Response $response;
     protected array $config;
 
-    public function __construct(EventService $events, array $config)
+    public function __construct(EventService $events, FilterService $filters, Response $response, array $config)
     {
         $this->events = $events;
+        $this->filters = $filters;
+        $this->response = $response;
         $this->config = $config;
+    }
+
+    /**
+     * Send API response.
+     *
+     * @param array $data
+     * @return void
+     */
+    public function respond(array $data): void
+    {
+        $this->response->sendJson($this->filters->doFilter('api.response', $data));
     }
 
 }
